587b28d56e26509a863b3b1a210799cf53b3e264
Artyom Yesayan <yesart8@gmail.com>
2025-10-29 20:46:38 -0400

    some more constants

--- src/StateMachineConstants.hpp
+++ src/StateMachineConstants.hpp
@@ -1,1 +1,9 @@
 constexpr float TAKEOFF_ACCEL_THRESHOLD = 0.0f;
+
+constexpr float BOOST_TIMEOUT = 0.0f;
+
+constexpr float COAST_TIMEOUT = 0.0f;
+
+constexpr float DROGUE_DESCENT_TIMEOUT = 0.0f;
+
+constexpr float MAIN_DESCENT_TIMEOUT = 0.0f;
